0481cbcc4897549c5c270e56f8e9ad3520d8ed4a
Manan Chauhan <78793593+Manan-2000@users.noreply.github.com>
2024-09-04 04:30:52 +0530

    Update app.py

--- Major_Project/app.py
+++ Major_Project/app.py
@@ -15,7 +15,6 @@ from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 import pickle
 
 # Load the preprocessed mapping and tokenizer
